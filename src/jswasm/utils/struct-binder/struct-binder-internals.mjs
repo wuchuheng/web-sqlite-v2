ceb9e9420d60fcfdcf3261984abe55a025e0833b
wuchuheng <root@wuchuheng.com>
2025-11-18 01:38:58 +0800

    refactor(struct-binder): Migrate struct-binder-signatures from JS to TypeScript #2

--- src/jswasm/utils/struct-binder/struct-binder-internals.mjs
+++ src/jswasm/utils/struct-binder/struct-binder-internals.mjs
@@ -6,6 +6,6 @@ export * from "./struct-binder-helpers/struct-binder-helpers.js";
 export * from "./struct-binder-context.mjs";
 export * from "./struct-binder-accessors.mjs";
 export * from "./struct-binder-struct-type.mjs";
-export * from "./struct-binder-signatures/struct-binder-signatures.js";
+export * from "./struct-binder-signatures/struct-binder-signatures";
 export * from "./struct-binder-memory/struct-binder-memory";
 export * from "./struct-binder-members.mjs";
